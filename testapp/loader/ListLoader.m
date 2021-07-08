91844237233ab344aed6d651fec6fced51095868
Naomiiiii0212 <41138766+Naomiiiii0212@users.noreply.github.com>
2021-07-08 10:23:23 +0800

    iOS中的JSON解析

--- testapp/loader/ListLoader.m
+++ testapp/loader/ListLoader.m
@@ -11,32 +11,36 @@
 @implementation ListLoader
 
 - (void) loadListData {
-//    NSString *urlString = @"https://www.baidu.com/";
-//    NSURL *listUrl = [NSURL URLWithString:urlString];
+    NSString *urlString = @"https://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeef798bad3d54e";
+    NSURL *listUrl = [NSURL URLWithString:urlString];
 
-	[[AFHTTPSessionManager manager] GET:@"https://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeef798bad3d54e" parameters:nil headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
-
-	 } success:^(NSURLSessionDataTask * _Nonnull task, id _Nullable responseObject) {
-	         NSLog(@"");
-	 } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-	         NSLog(@"");
-	 }];
-
-
-
-
-
-//    //__unused NSURLRequest *listRequest = [NSURLRequest requestWithURL:listUrl];
+//	[[AFHTTPSessionManager manager] GET:@"https://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeef798bad3d54e" parameters:nil headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
 //
-//    NSURLSession *session = [NSURLSession sharedSession];
-//
-//    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listUrl completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-//        NSLog(@" ");
-//    }];
-//
-//    [dataTask resume];
-//
-//    NSLog(@"");
+//	 } success:^(NSURLSessionDataTask * _Nonnull task, id _Nullable responseObject) {
+//	         NSLog(@"");
+//	 } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//	         NSLog(@"");
+//	 }];
+
+
+
+
+
+    //__unused NSURLRequest *listRequest = [NSURLRequest requestWithURL:listUrl];
+
+    NSURLSession *session = [NSURLSession sharedSession];
+
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listUrl completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+       
+        NSError *jsonError;
+        id jsonObj = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        
+        NSLog(@" ");
+    }];
+
+    [dataTask resume];
+
+    NSLog(@"");
 }
 
 @end
